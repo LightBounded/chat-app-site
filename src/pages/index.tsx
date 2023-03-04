ab54086e7336275d6860bf1bf1ac787aa23d52f9
Caleb Rivera <calebrossr@gmail.com>
2023-03-04 13:41:46 -0500

    Add simple chat layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import Layout from "@/components/Layout";
-import { Inter } from "next/font/google";
-
 
 export default function Home() {
   return (
-    <Layout title="Home">
-      <div>Hello</div>
+    <Layout title="Chat Room">
+      <div className="flex min-h-screen">
+        <div className="flex-[5] flex flex-col py-6 px-4">
+          <div>Asd</div>
+          <input
+            className="mt-auto w-full bg-gray-800 rounded py-3 px-4"
+            type="text"
+            placeholder="Message"
+          />
+        </div>
+        <div className="flex-1 bg-gray-900"></div>
+      </div>
     </Layout>
   );
 }
