1669c7b761edced3591e9f1cd667bd476315d7e5
Caleb Rivera <calebrossr@gmail.com>
2023-03-04 00:20:55 -0500

    First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+import { Inter } from "next/font/google";
+
+
+export default function Home() {
+  return (
+    <Layout title="Home">
+      <div>Hello</div>
+    </Layout>
+  );
+}
