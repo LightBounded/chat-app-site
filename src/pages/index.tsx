212b502155a5d8c152f76f368873730c56a2d27c
Caleb Rivera <calebrossr@gmail.com>
2023-03-04 16:09:09 -0500

    Fix chat layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,22 @@ import Layout from "@/components/Layout";
 export default function Home() {
   return (
     <Layout title="Chat Room">
-      <div className="flex min-h-screen">
-        <div className="flex-[5] flex flex-col py-6 px-4">
-          <div>Asd</div>
-          <input
-            className="mt-auto w-full bg-gray-800 rounded py-3 px-4"
-            type="text"
-            placeholder="Message"
-          />
+      <div className="flex max-h-screen">
+        <div className="flex-[1]"></div>
+        <div className="flex flex-[5] flex-col py-6 px-4">
+          {/* Messages */}
+          <div className="flex-1 overflow-y-scroll">
+            {[...Array(40)].map((_, i) => (
+              <div key={i}>Hello</div>
+            ))}
+          </div>
+          <form className="">
+            <input
+              className="mt-4 w-full rounded bg-gray-800 py-3 px-4"
+              type="text"
+              placeholder="Message"
+            />
+          </form>
         </div>
         <div className="flex-1 bg-gray-900"></div>
       </div>
